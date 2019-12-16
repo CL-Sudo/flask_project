c9332d45dda149b8e0ac7505ca4fa2f251d51772
CL-Sudo <acl0002@protonmail.ch>
2019-12-16 15:53:53 +0800

    Learned template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World!"
+    user = {'username':'Miguel'}
+    posts = [
+        {
+            "author": {"username": "John", "gender":"male"},
+            "body": "Beautiful day in Portland!"
+        },
+
+        {
+            "author": {"username": "Susan", "gender":"female"},
+            "body": "The movie was so damn cool!"
+        }
+    ]
+    return render_template("index.html", 
+                            title = "Home", 
+                            user = user, 
+                            posts = posts)
